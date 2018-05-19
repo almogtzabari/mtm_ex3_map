4f1feec937917511567f59f4fc4cc63445b513d5
almogtzabari <almogtzabari@campus.technion.ac.il>
2018-05-19 16:02:11 +0300

    node.c update

--- node.c
+++ node.c
@@ -17,21 +17,24 @@ struct node_t{
 //                        STRUCT NODE FUNCTIONS                          //
 //-----------------------------------------------------------------------//
 
-/** Checked
+/**
  ***** Function: nodeCreate *****
  * Description: Creates a new node.
- * @param data - a pointer to data.
- * @param key - a pointer to key.
- * @param copyDataElement - a pointer to copy data function.
- * @param copyKeyElement - a pointer to copy key function.
- * @param freeDataElement - a pointer to destroy data function.
- * @param freeKeyElement - a pointer to destroy key function.
- * @return - A pointer to the new node.
+ * @param data - The data element which need to be assigned to the new
+ * node.
+ * @param key - The key element which need to be assigned to the new node.
+ * @param copyDataElement - Function pointer to be used for copying data
+ * elements into the node.
+ * @param copyKeyElement - Function pointer to be used for copying key
+ * elements into the node.
+ * @param freeKeyElement - Function pointer to be used for removing key
+ * element from the node. This free function will be used in case of
+ * memory allocation fail of the data copy function.
+ * @return - The new node.
  */
 Node nodeCreate(MapDataElement data, MapKeyElement key,
                 copyMapDataElements copyDataElement,
                 copyMapKeyElements copyKeyElement,
-                freeMapDataElements freeDataElement,
                 freeMapKeyElements freeKeyElement){
     Node new_node = malloc(sizeof(*new_node));
     if(!new_node){
@@ -54,13 +57,14 @@ Node nodeCreate(MapDataElement data, MapKeyElement key,
     return new_node;
 }
 
-/** Checked
+/**
  ***** Function: nodeDestroy *****
  * Description: Frees all allocated memory of the given node.
- * @param node - a pointer to a node.
- * @param freeDataElement - a pointer to a data destroy function.
- * @param freeKeyElement - a pointer to a key destroy function.
- * @return
+ * @param node - The node we want to destroy.
+ * @param freeDataElement - Function pointer to be used for removing data
+ * element from the node.
+ * @param freeKeyElement - Function pointer to be used for removing key
+ * element from the node.
  */
 void nodeDestroy(Node node, freeMapDataElements freeDataElement,
                        freeMapKeyElements freeKeyElement){
@@ -69,10 +73,11 @@ void nodeDestroy(Node node, freeMapDataElements freeDataElement,
     free(node);
 }
 
-/** Checked
+/**
  ***** Function: nodeGetKey *****
- * Description: Gets a node in the map and returns node's key.
- * @param node - a pointer to a node.
+ * Description: Gets a node and returns node's key.
+ * NOTICE: THE ORIGINAL KEY IS RETURNED.
+ * @param node - The node which we want to get its key.
  * @return - Node's key element.
  */
 MapKeyElement nodeGetKey(Node node){
@@ -80,14 +85,15 @@ MapKeyElement nodeGetKey(Node node){
     return node->key;
 }
 
-/** todo: fix bug!
+/**
  ***** Function: nodeGetData *****
  * Description: Gets a node and a copy data function and returns a copy of
  * node's data element.
- * Notice: User is responsible for destroying the data copy.
- * @param node - a pointer to a node.
- * @param copyDataElement - a pointer to a copy function of the data.
- * @return - A pointer to a copy of node's data.
+ * NOTICE: USER IS RESPONSIBLE FOR DESTROYING THE DATA COPY.
+ * @param node - The node which we want to get a copy of its data.
+ * @param copyDataElement - Function pointer to be used for copying the
+ * data element of the node.
+ * @return - A copy of the given node's data.
  */
 MapDataElement nodeGetData(Node node,copyMapDataElements copyDataElement){
     if(!node){
@@ -104,24 +110,22 @@ MapDataElement nodeGetData(Node node,copyMapDataElements copyDataElement){
     return data_copy;
 }
 
-/** Checked
+/**
  ***** Function: nodeGetNext *****
- * Descritpion: Returns the next node (the node after the
- * given node).
- * @param map - a pointer to a map.
- * @param node - a pointer to a node.
- * @return - Pointer to the node after the given node.
+ * Descritpion: Returns the next node of the given node.
+ * @param node - The node which we want to find its next node.
+ * @return - The next node.
  */
 Node nodeGetNext(Node node){
     return node->next;
 }
 
-/** Checked
+/**
  ***** Function: nodeSetNext *****
- * Description: Gets node and next_node and sets node's 'next'
- * to be next_node.
- * @param node
- * @param next_node
+ * Description: Gets a node and a next_node and sets node's next
+ * to be 'next_node'.
+ * @param node - The node which we want to change its 'next'.
+ * @param next_node - The node which we want to be pointed at by 'node'
  * @return - Status success/failure of function.
  */
 NodeResult nodeSetNext(Node node, Node next_node){
@@ -133,15 +137,18 @@ NodeResult nodeSetNext(Node node, Node next_node){
     return NODE_SUCCESS;
 }
 
-/** Checked
+/**
  ***** Function: nodeSetData *****
  * Description: The function gets a node and a new data and creates a copy
  * of the data. The copy of the new data will be inserted to node's data.
- * Notice: Node's old data will be destroyed.
- * @param node - a pointer to the node which we want to modify its data.
- * @param new_data - a pointer to the new data.
- * @param copyDataElement - a pointer to the copy data function.
- * @return
+ * Node's old data will be destroyed.
+ * @param node - The node which we want to modify its data.
+ * @param new_data - The new data to insert into the node.
+ * @param copyDataElement - Pointer to the copy data element function.
+ * Will be used to create a copy of the given new data.
+ * @param freeDataElement - Pointer to the free data element function.
+ * Will be used to destroy node's old data.
+ * @return - Status success/failure of the function.
  */
 NodeResult nodeSetData(Node node, MapDataElement new_data,
                        copyMapDataElements copyDataElement,
